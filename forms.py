4677245bb81d1b7acb25974e3a1d3821dc733869
Kaleb Burnham <kaleb.burnham@usda.gov>
2020-01-03 14:06:48 -0600

    Combined Nested Loop and Starp websites.

---
+++ forms.py
@@ -0,0 +1,166 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, FloatField, BooleanField, IntegerField, SubmitField, StringField
+from wtforms import validators
+from wtforms.validators import InputRequired, Length, ValidationError, NumberRange
+
+from nestedloop.nestedloop import (TEMPERATURE_MIN, TEMPERATURE_MAX,
+    NUM_TO_RETURN_MIN, PCR_MINIMUM, PCR_MAXIMUM, REF_SEQ_MIN_LENGTH,
+    REF_SEQ_MAX_LENGTH, F_FROM_MIN, F_TO_MIN, R_FROM_MIN, R_TO_MIN)
+
+class StarpForm(FlaskForm):
+    input_data = TextAreaField('input_data',
+                                  validators=[InputRequired(),
+                                              Length(min=50, max=20000, message='input_data must be between 50 and 20,000 characters.')])
+    submit = SubmitField('Submit')
+
+# DEFAULTS
+NUM_TO_RETURN_DEFAULT=5
+PCR_MIN_DEFAULT=3000
+PCR_MAX_DEFAULT=8000
+TM_MIN_DEFAULT=54.0
+TM_OPT_DEFAULT=56.0
+TM_MAX_DEFAULT=58.0
+F_FROM_DEFAULT=1
+F_TO_DEFAULT=1
+R_FROM_DEFAULT=1
+R_TO_DEFAULT=1
+
+class ValidateTemperature(object):
+    def __init__(self, 
+            min=TEMPERATURE_MIN, 
+            max=TEMPERATURE_MAX, 
+            message=("Temperature must be between " 
+                + str(TEMPERATURE_MIN) + " and " 
+                + str(TEMPERATURE_MAX) + " degrees.")):
+        self.min = min
+        self.max = max
+        self.message = message
+
+    def __call__(self, form, field):
+        tm = field.data
+        ''' Data is None if its of the wrong type. '''
+        if tm is None:
+            return
+
+        if tm < self.min or tm > self.max:
+            raise ValidationError(self.message)
+
+class ValidateReferenceSequence(object):
+    def __init__(self, 
+            min=REF_SEQ_MIN_LENGTH, 
+            max=REF_SEQ_MAX_LENGTH, 
+            message='Reference sequence must be between 100 and 20,000 characters.'):
+
+        self.min = min
+        self.max = max
+        self.message = message
+
+    def __call__(self, form, field):
+        """ It's ok if this field is empty as long as both primers
+        are entered. """
+
+        if form.forward_primer.data and form.reverse_primer.data:
+            print(form.forward_primer)
+            return
+
+        seq = field.data
+        if len(seq) < self.min or len(seq) > self.max:
+            raise ValidationError(self.message)
+
+class NestedLoopForm(FlaskForm):
+    ref_sequence = TextAreaField(
+                        'Reference Sequence for NL-PCR Primer Design',
+                        validators=[ValidateReferenceSequence()],
+
+                        render_kw={'maxlength': REF_SEQ_MAX_LENGTH})
+
+    non_targets = TextAreaField('Non-specific alignments')
+
+    specificity_checking = BooleanField('Specificity Checking')
+
+    f_from = IntegerField('Region for forward primer:',
+                          validators=[InputRequired(), 
+                                      NumberRange(min=F_FROM_MIN, message="Regions must be positive numbers.")],
+                          default=F_FROM_DEFAULT,
+                          render_kw={'type': 'number',
+                                     'min': F_FROM_MIN,
+                                     'required': 'true'})
+
+    f_to = IntegerField('To',
+                        validators=[InputRequired(), 
+                                    NumberRange(min=F_TO_MIN, message="Regions must be positive numbers.")],
+                        default=F_TO_DEFAULT,
+                        render_kw={'type': 'number',
+                                   'min': F_TO_MIN,
+                                   'required': 'true'})
+
+    r_from = IntegerField('Region for reverse primer:',
+                          validators=[InputRequired(), 
+                                      NumberRange(min=R_FROM_MIN, message="Regions must be positive numbers.")],
+                          default=R_FROM_DEFAULT,
+                          render_kw={'type': 'number',
+                                     'min': R_FROM_MIN,
+                                     'required': 'true'})
+
+    r_to = IntegerField('To',
+                        validators=[InputRequired(), 
+                                    NumberRange(min=R_TO_MIN, message="Regions must be positive numbers.")],
+                        default=R_TO_DEFAULT,
+                        render_kw={'type': 'number',
+                                   'min': R_TO_MIN,
+                                   'required': 'true'})
+
+    tm_min = FloatField('Tm Min',
+                        validators=[InputRequired(), ValidateTemperature()],
+                        default=TM_MIN_DEFAULT,
+                        render_kw={'type': 'number',
+                                   'step': 'any',
+                                   'min': TEMPERATURE_MIN,
+                                   'max': TEMPERATURE_MAX,
+                                   'required': 'true'})
+
+    tm_opt = FloatField('Tm Opt',
+                        validators=[InputRequired(), ValidateTemperature()],
+                        default=TM_OPT_DEFAULT,
+                        render_kw={'type': 'number',
+                                   'step': 'any',
+                                   'min': TEMPERATURE_MIN,
+                                   'max': TEMPERATURE_MAX,
+                                   'required': 'true'})
+
+    tm_max = FloatField('Tm Max',
+                        validators=[InputRequired(), ValidateTemperature()],
+                        default=TM_MAX_DEFAULT,
+                        render_kw={'type': 'number',
+                                   'step': 'any',
+                                   'min': TEMPERATURE_MIN,
+                                   'max': TEMPERATURE_MAX,
+                                   'required': 'true'})
+
+    num_to_return = IntegerField('Number to Return',
+                                 validators=[InputRequired(),
+                                             NumberRange(min=NUM_TO_RETURN_MIN, 
+                                                         message="Must return at least " + str(NUM_TO_RETURN_MIN) + " primer!")],
+                                 default=NUM_TO_RETURN_DEFAULT,
+                                 render_kw={'type': 'number',
+                                            'min': NUM_TO_RETURN_MIN,
+                                            'required': 'true'})
+
+    pcr_min = IntegerField('PCR Min',
+                           default=PCR_MIN_DEFAULT,
+                           render_kw={'type': 'number',
+                                      'min': PCR_MINIMUM,
+                                      'max': PCR_MAXIMUM,
+                                      'required': 'true'})
+
+    pcr_max = IntegerField('PCR Max',
+                           default=PCR_MAX_DEFAULT,
+                           render_kw={'type': 'number',
+                                      'min': PCR_MINIMUM,
+                                      'max': PCR_MAXIMUM,
+                                      'required': 'true'})
+
+    forward_primer = StringField('Forward Primer')
+    reverse_primer = StringField('Reverse Primer')
+    submit = SubmitField('Find Primers')
+    nested_loop_error = None
---
+++ routes.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template, redirect, session, request
+from forms import StarpForm, NestedLoopForm
+from flask_session import Session
+
+import sys
+import os
+
+from starp.starp2 import Starp2
+from starp.exceptions import StarpError
+
+from nestedloop.nestedloop import NestedLoop
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(16)
+""" By default, a maximum 500 sessions are stored at once. """
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
+
+@app.route('/')
+def go_to_homepage():
+    return redirect('/starp')
+
+@app.route('/starp')
+def show_form():
+    form = StarpForm()
+    return render_template('/starp.html', form=form)
+
+@app.route('/starp', methods=['POST'])
+def starp_post():
+    form = StarpForm()
+
+    if 'snp' in request.form:
+        """ User has chosen their SNP. Compute primers. """
+        chosen_snp_descriptor = request.form['snp']
+        snp = next((x for x in session['starp'].snps 
+            if x.descriptor == chosen_snp_descriptor), None)
+        try:
+            session['starp'].choose_snp(snp)
+        except StarpError as e:
+            form.errors['starp'] = [e.message]
+            return render_template('starp.html', form=form)
+        return render_template('starp.html', form=form, starp=session['starp'])
+    else:
+        """ User has submitted the initial data. Find the SNPS, and ask
+        the user which one to design primers around. """
+        try:
+            starp = Starp2(form.input_data.data)
+            session['starp'] = starp
+            snp_gui = starp.html()
+        except StarpError as e:
+            form.errors['starp'] = [e.message]
+            return render_template('starp.html', form=form)
+        return render_template('starp.html', form=form, snp_gui=snp_gui)
+
+@app.route('/nestedloop')
+def go_to_nestedloop():
+    form = NestedLoopForm()
+    return render_template('/nestedloop.html', form=form)
+
+@app.route('/nestedloop', methods=['POST'])
+def create_pairs():
+    form = NestedLoopForm()
+    if not form.validate_on_submit():
+        return render_template('nestedloop.html', form=form)
+
+    session['nl'] = NestedLoop(form.ref_sequence.data,
+                               (form.tm_min.data, form.tm_opt.data, form.tm_max.data),
+                               form.f_from.data,
+                               form.f_to.data,
+                               form.r_from.data,
+                               form.r_to.data,
+                               form.pcr_min.data,
+                               form.pcr_max.data,
+                               form.num_to_return.data,
+                               form.forward_primer.data,
+                               form.reverse_primer.data,
+                               form.non_targets.data)
+
+    try:
+        session['nl'].run()
+        #session['nl'].create_pairs()
+    except NestedLoopError as e:
+        form.errors["NL"] = [e.message]
+        return render_template('nestedloop.html', **locals())
+
+    pairs = session['nl'].pairs
+    nl = session['nl']
+    return render_template('nestedloop.html', form=form, pairs=pairs)
+
+@app.route('/downloadCSV')
+def download_csv():
+    nl = session['nl']
+    csv = "#,Type,Sequence (5'->3'),Strand,Start,End,Tm (Celsius),GC Content,Amplicon Length,Additive,Loop 2 Program,Loop 2 Cycle Number\n"
+    csv += "\n"
+    number = 1
+    for pair in nl.pairs:
+        pair_csv = (str(number) + ',Forward,' + pair.forward_primer.sequence + ',Plus,' + str(pair.forward_primer.start) + ',' + str(pair.forward_primer.end) + ','
+                        + str(pair.forward_primer.tm) + ',' + str(pair.forward_primer.gc) + ',' + str(pair.distance) + ',' 
+                        + pair.additive().additive + ',' + pair.additive().pcr_temperatures.replace('\n', ' ') + ',' + str(pair.additive().n) + '\n')
+        pair_csv += (',Reverse,' + pair.reverse_primer.sequence + ',Minus,' + str(pair.reverse_primer.start) + ',' + str(pair.reverse_primer.end)
+                        + ',' + str(pair.reverse_primer.tm) + ',' + str(pair.reverse_primer.gc) + '\n\n')
+        csv += pair_csv
+        number += 1
+
+    return Response(csv, 
+                    mimetype="text/csv", 
+                    headers = {"Content-disposition":
+                               "attachment; filename=primers.csv"})
+
+if __name__ == "__main__":
+    #sys.path.append('./starp')
+    app.run(debug=True)
